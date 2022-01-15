f0e0198aafaa907244bcd55f3d71fe2e14d0e356
SilvaniaR <Silvania.roche@outlook.com>
2022-01-15 12:52:56 -0500

    Addingthe election audit code.

---
+++ Python_practice.py
@@ -0,0 +1,15 @@
+counties = ["Arapahoe", "Denver","Jefferson"]
+if counties [1] =='Denver':
+    print(counties[1])  
+if counties[3]!='Jefferson'
+    print(counties[2])
+
+temperature = int(input("What is the temperature outside?"))
+
+if temperature > 80:
+        print("Turn on AC.") 
+    
+ else:
+        print("Open the windows.")
+
+
